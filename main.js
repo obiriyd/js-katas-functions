0cee4af5f822fd726f39ffbb71182917e7a40be2
Desmond Obiri-Yeboah <dobiriyeboah@hotmail.com>
2020-05-29 07:06:04 -0400

    Final commits

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 function add(a,b) {
     return a+b;
 }
+//Output Addition
 console.log('Kata 1 - Addition: ' + add(6,5));
 
 
@@ -10,10 +11,11 @@ console.log('Kata 1 - Addition: ' + add(6,5));
 function multiply(a,b) {
     let product = 0;
     for (ctr1 = 0; ctr1 < b; ctr1++) {
-        product += add(a,0);
+        product = add(product, add(a,0));
     }
     return product;
 }
+//Output Product
 console.log('Kata 2 - Product: ' + multiply(2,5))
 
 
@@ -22,37 +24,42 @@ console.log('Kata 2 - Product: ' + multiply(2,5))
 function power(x,n) {
     let power = 1;
     for (ctr2 = 0; ctr2 < n; ctr2++) {
-        power *= multiply(x,1);
+        power = multiply(power, multiply(x,1));
     }
     return power;
 }
+//Output Power
 console.log('Kata 3 - Power: ' + power(2,5))
 
 
 
 //Kata 4 - Factorial
 function factorial(f) {
-    let factorial = f;
+    let fact = f;
     for (ctr3 = 1; ctr3 < f; ctr3++) {
-        factorial *= multiply(ctr3,1);
+        fact = multiply(fact, multiply(ctr3,1));
     }
-    return factorial;
+    return fact;
 }
+//Output Factorial
 console.log('Kata 4 - Factorial: ' + factorial(5))
 
 
 //Kata Bonus - Fibonacci
 function fibonacci(n) {
+    //Start an array of first 2 Fibonacci sequence
     let arr = [0, 1];
-  
+
+    //Return first 3 values
+    if (n < 0 || n == 0) return 'Invalid Input';
+    if (n == 1) return 0;
     if (n <= 2) return 1;
-  
+
+    //Calculate rest of sequence if input greater than 3
     for (let i = 2; i <= n; i++) {
       arr[i] = add(arr[add(i,-1)],arr[add(i,-2)]);
     }
-  
-    return arr[add(n, -1)];
-  }
-
-
-console.log('Kata Bonus - Fibonaci: ' + fibonacci(8));
+    return arr[add(n, -1)]; //Return position taking into account possition 0 of the array
+}
+//Output Fibonacci
+console.log('Kata Bonus - Fibonaci: ' + fibonacci(3));
